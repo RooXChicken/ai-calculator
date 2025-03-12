af1d0388526a48bfdf84f49055d66afac8512309
RooXChicken <rooxchicken@gmail.com>
2025-03-12 13:17:16 -0400

    add "support" for other operations (it does NOT work)

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as tf from "@tensorflow/tfjs-node";
 import * as readline from "node:readline";
 
-const size = 512;
+const size = 1024*2;
 
 const input = readline.createInterface({
     input: process.stdin,
@@ -9,22 +9,27 @@ const input = readline.createInterface({
 });
 
 const model = tf.sequential();
-model.add(tf.layers.dense({ units: 1, inputShape: [2] }));
-model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
+model.add(tf.layers.dense({ units: 1, inputShape: [3] }));
+model.compile({ loss: tf.losses.absoluteDifference, optimizer: "adam" });
 
 let _data = generateTestData();
 
-const _input = tf.tensor(_data[0], [size, 2]);
+const _input = tf.tensor(_data[0], [size, 3]);
 const _answers = tf.tensor(_data[1], [size, 1]);
 
-await model.fit(_input, _answers, { epochs: 250 });
+await model.fit(_input, _answers, { epochs: 64 });
 
 getInput();
 
 function getInput() {
-    input.question("Please input your calculation!", (_input) => {
-        let _nums = _input.split("+");
-        console.log(_input + " = " + model.predict(tf.tensor2d([parseInt(_nums[0].trim()), parseInt(_nums[1].trim())], [1, 2])).dataSync()[0]);
+    input.question("Please input your calculation! ", (_input) => {
+        let _nums = _input.split(RegExp("(\\+|-|\\*|\\/)"));
+        let _operation = (_nums[1] == "+") ? 0 : (_nums[1] == "-") ? 1 : (_nums[1] == "*") ? 2 : 3;
+
+        let _in = [parseInt(_nums[0].trim()), parseInt(_nums[2].trim()), _operation];
+        console.log(_in);
+        let _tensor = tf.tensor(_in, [1, 3]);
+        console.log(_input + " = " + model.predict(_tensor).dataSync()[0]);
 
         getInput();
     });
@@ -35,13 +40,28 @@ function generateTestData() {
     let _answers = [];
 
     for(let i = 0; i < size; i++) {
-        let _one = (Math.random() * 10);
-        let _two = (Math.random() * 10);
-        let _ans = _one + _two;
+        let _one = (Math.random() * 100);
+        let _two = (Math.random() * 100);
+        let _operation = Math.floor(Math.random()*3);
 
-        _input[_input.length] = [_one, _two];
+        let _ans;
+        switch(_operation) {
+            case 0: _ans = _one + _two; break;
+            case 1: _ans = _one - _two; break;
+            case 2: _ans = _one * _two; break;
+            case 3: _ans = _one / _two; break;
+        }
+
+        _input[_input.length] = [_one, _two, _operation];
         _answers[_answers.length] = _ans;
     }
 
     return [_input, _answers];
 }
+
+// function loss(_input, _output) {
+//     _input.print();
+//     _output.print();
+
+//     return 0;
+// }
